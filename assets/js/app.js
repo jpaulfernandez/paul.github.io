9b594f119dc839924f70f560e2e547c43bd8de05
Jan Paul M. Fernandez <jpaul.fernandez18@gmail.com>
2017-10-05 22:53:53 +0800

    Added scroll animation

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+(function(){
+
+	'use strict';
+
+	window.sr = ScrollReveal({reset: true});
+	sr.reveal('.about-section');
+	sr.reveal('.portrait-section');
+	sr.reveal('.work-section');
+	sr.reveal('.project-section');
+	sr.reveal('.arsenal-section');
+	sr.reveal('.photog-section');
+	sr.reveal('.talk-section');
+	sr.reveal('.footer-section');
+})();
